b157fbe46ddf7c8ab781bcb2436fc1b53441f95a
charleswayo <charleswayo@gmail.com>
2024-01-08 20:59:29 +0900

    add patient view, url file

--- patient/views.py
+++ patient/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+from patient.models import Patient
+from patient.serializers import PatientSerializer
+
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+    def create(self, request):
+        patient_serializer = PatientSerializer(data=request.data)
+        if patient_serializer.is_valid():
+            patient_serializer.save()
+            return Response(patient_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(patient_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
